c36fa9cec7963201e9b87f4861a41aa76181db97
pi <ieee2018@ieee2018-desktop>
2018-03-01 23:45:28 -0600

    Implemented edge detection

--- Arduino/ArduinoSource/lib/ProximitySensor/ProximitySensor.cpp
+++ Arduino/ArduinoSource/lib/ProximitySensor/ProximitySensor.cpp
@@ -1,5 +1,28 @@
 #include "ProximitySensor.h"
+#include <Arduino.h>
 
-ProximitySensor::ProximitySensor() {}
+ProximitySensor::ProximitySensor(int t, int e) : ProximitySensor(t, e, 500) {}
 
-bool ProximitySensor::edgeDetected() const {return false;}
+ProximitySensor::ProximitySensor(int t, int e, float c) : trig_pin(t), echo_pin(e), cutoff(c){
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+}
+
+void ProximitySensor::updateProximity() {
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+  proximity = pulseIn(echo_pin, HIGH);
+}
+
+float ProximitySensor::getProximity() {
+  updateProximity();
+  return proximity;
+}
+
+bool ProximitySensor::edgeDetected() {
+  updateProximity();
+  return proximity > cutoff;
+}
