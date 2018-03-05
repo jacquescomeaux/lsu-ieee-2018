3183673e81c837516075150d49c71863aefd96a1
Jacques Comeaux <jcome35@lsu.edu>
2018-03-05 16:51:55 -0600

    Wheel

--- Arduino/ArduinoSource/lib/Wheel/Wheel.h
+++ Arduino/ArduinoSource/lib/Wheel/Wheel.h
@@ -2,10 +2,12 @@
 #define WHEEL_H
 
 #include <Adafruit_MotorShield.h>
+#include <Encoder.h>
 
 class Wheel {
   private:
     Adafruit_DCMotor* motor;
+    Encoder encoder;
     Fixed speed, goal_speed;
     bool direction_set;
     const Fixed tolerance, max_speed, min_speed;
