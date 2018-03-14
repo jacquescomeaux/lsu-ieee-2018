16e50fc8ac93e740fc8fea79ad5400d72810e0cf
Jacques Comeaux <jcome35@lsu.edu>
2018-03-14 18:01:31 -0500

    LineSensor test attempted fix

--- Arduino/SensorCalibration/LineSensors/src/sketch.ino
+++ Arduino/SensorCalibration/LineSensors/src/sketch.ino
@@ -3,22 +3,19 @@
 const int NUM_SENSORS = 32;
 const unsigned char PIN_START = 22;
 QTRSensorsRC* qtrrc1, qtrrc2;
-unsigned int values1[NUM_SENSORS/2];
-unsigned int values2[NUM_SENSORS/2];
-unsigned char pins1[NUM_SENSORS/2];
-unsigned char pins2[NUM_SENSORS/2];
+unsigned int values[NUM_SENSORS];
+unsigned char pins[NUM_SENSORS];
 
 void setup() {
-  for(unsigned char i = 0; i < NUM_SENSORS/2; i++) pins1[i] = static_cast<unsigned char>(PIN_START + i);
-  for(unsigned char i = 0; i < NUM_SENSORS/2; i++) pins2[i] = static_cast<unsigned char>(PIN_START + NUM_SENSORS/2 + i);
-  qtrrc1 = new QTRSensorsRC(pins1, NUM_SENSORS/2);
-  qtrrc2 = new QTRSensorsRC(pins2, NUM_SENSORS/2);
+  for(unsigned char i = 0; i < NUM_SENSORS; i++) pins1[i] = static_cast<unsigned char>(PIN_START + i);
+  qtrrc1 = new QTRSensorsRC(pins, NUM_SENSORS/2);
+  qtrrc2 = new QTRSensorsRC(&pins[NUM_SENSORS/2], NUM_SENSORS/2);
  Serial.begin(9600);
 }
 
 void loop() {
-  qtrrc1->read(values1);
-  qtrrc2->read(values2);
+  qtrrc1->read(values);
+  qtrrc2->read(&values[NUM_SENSORS/2]);
   for(int i = 0; i < 4; i++) {
     for(int j = 0; j < 8; j++) {
       int k = i*8+j;
@@ -26,8 +23,8 @@ void loop() {
       if(k<10) Serial.print("0");
       Serial.print(k);
       Serial.print(": p");
-      Serial.print((k<16) ? pins1[k] : pins2[k-16]);
-      unsigned int v = (k<16) ? values1[k] : values2[k-16];
+      Serial.print(pins[k]);
+      unsigned int v = values[k];
       Serial.print(" v: ");
       if(v < 1000) Serial.print(" "); 
       if(v < 100) Serial.print(" "); 
